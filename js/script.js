b3f464d435d599c0937e1a9d7960f0e20ffe4c2b
amkosh <amkosh@mail.ru>
2022-05-18 15:46:37 +0500

    +add animation autoMove

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ unitCellX.push(x);
 unitCellY.push(y);
 let unitSize = 1;
 let fail = false;
+let isPause = false;
+
+let direction = 'ArrowUp';
+let speed = 10;
+
+// следим за кадрами анимации, чтобы если что — остановить игру
+let rAF = null; 
+//Счетчик кадров
+let count = 0;
 
 for(let i = 0; i < 20; i++) {
     field[i] = [20];
@@ -24,7 +33,17 @@ initItems();
 initField();
 drawField();
 addItem();
-document.body.addEventListener("keydown", move);
+document.body.addEventListener("keydown", controls);
+
+function controls(event){
+    console.log(event);
+    if(event.key == ' '){
+        pause();
+    } else if (isPause == true) {
+        move(event.key);
+    }
+    
+}
 
 function initItems() {
     for(let i = 0; i < 20; i++) {
@@ -79,6 +98,7 @@ function colCheck(){
     if(x < 0 || x > 19 || y < 0 || y > 19) {
         fail = true;
         info.parentElement.innerHTML = 'GAME OVER!'
+        cancelAnimationFrame(rAF);
     } //Проверка на предмет
     else if(items[x][y]){
         addItem();
@@ -91,6 +111,7 @@ function colCheck(){
     else if (field[x][y].className == "unit") {
         fail = true;
         info.parentElement.innerHTML = 'GAME OVER!'
+        cancelAnimationFrame(rAF);
     }
 }
 
@@ -110,17 +131,18 @@ function initUnit() {
 
 function move(event){
     if(fail == false){
-        switch(event.key){
-            case 'ArrowUp': --x;
+        switch(event){
+            case 'ArrowUp': --x;    direction = event;
             break;
-            case 'ArrowDown': ++x;
+            case 'ArrowDown': ++x;  direction = event;
             break;
-            case 'ArrowRight': ++y;
+            case 'ArrowRight': ++y; direction = event;
             break;
-            case 'ArrowLeft': --y;
+            case 'ArrowLeft': --y;  direction = event;
             break;
             default: return;
         }
+        count = 0;
         colCheck();
         unitCellX.unshift(x);
         unitCellY.unshift(y);
@@ -136,4 +158,28 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function loop() {
+    // начинаем анимацию
+    rAF = requestAnimationFrame(loop);
+    // фигура сдвигается вниз каждые 35 кадров
+    if (++count > speed) {
+		//Движение змеи
+        move(direction);
+      count = 0;
+    }
+}
 
+function pause() {
+    let button = document.getElementById('pause');
+    isPause = !isPause;
+    if(isPause){
+        rAF = requestAnimationFrame(loop);
+        button.innerText = 'PAUSE';
+    } else {
+        cancelAnimationFrame(rAF);
+        button.innerText = 'RESUME';
+    }
+    
+}
+
+//rAF = requestAnimationFrame(loop);
