82c5e5a78f89c85518bb85873da93e273fc015da
amkosh <amkosh@mail.ru>
2022-05-25 16:49:20 +0500

    +touch control

--- js/script.js
+++ js/script.js
@@ -735,3 +735,62 @@ function cGen(){
         default: break;
     }
 }
+
+//SWIPE CONTROLS
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            if(!fail && (isPause == true)){
+                snake.moving = true;
+                snake.move('ArrowLeft');
+            }
+        } else {
+            if(!fail && (isPause == true)){
+                snake.moving = true;
+                snake.move('ArrowRight');
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+            if(!fail && (isPause == true)){
+                snake.moving = true;
+                snake.move('ArrowUp');
+            }
+        } else {
+            if(!fail && (isPause == true)){
+                snake.moving = true;
+                snake.move('ArrowDown');
+            }
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
